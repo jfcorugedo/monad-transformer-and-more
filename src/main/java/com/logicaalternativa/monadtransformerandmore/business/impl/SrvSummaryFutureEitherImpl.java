f232cd1c024e393e062767bcc51e95a186a4846d
mpu8l0000005 <jfcorugedo@gmail.com>
2018-04-24 15:22:27 +0200

    exercise solved

--- src/main/java/com/logicaalternativa/monadtransformerandmore/business/impl/SrvSummaryFutureEitherImpl.java
+++ src/main/java/com/logicaalternativa/monadtransformerandmore/business/impl/SrvSummaryFutureEitherImpl.java
@@ -6,11 +6,9 @@ import java.util.stream.Collectors;
 
 import static com.logicaalternativa.monadtransformerandmore.monad.MonadFutEitherWrapper.*;
 
+import com.logicaalternativa.monadtransformerandmore.bean.*;
 import scala.concurrent.Future;
 import scala.util.Either;
-import com.logicaalternativa.monadtransformerandmore.bean.Chapter;
-import com.logicaalternativa.monadtransformerandmore.bean.Sales;
-import com.logicaalternativa.monadtransformerandmore.bean.Summary;
 import com.logicaalternativa.monadtransformerandmore.business.SrvSummaryFutureEither;
 import com.logicaalternativa.monadtransformerandmore.errors.Error;
 import com.logicaalternativa.monadtransformerandmore.errors.impl.MyError;
@@ -50,7 +48,50 @@ public class SrvSummaryFutureEitherImpl implements SrvSummaryFutureEither<Error>
 
 		//TODO: Code the logic to build a Future<Either<Error, Summary>>
 
-		return null;
+		Future<Either<Error, Book>> book = srvBook.getBook(idBook);
+		Future<Either<Error, Optional<Sales>>> sales = wrap(srvSales.getSales(idBook), m)
+				.map(s -> Optional.of(s))
+				.recover(error -> Optional.empty()).value();
+		Future<Either<Error, Author>> author = wrap(book, m).flatMap(b -> srvAuthor.getAuthor(b.getIdAuthor())).value();
+		Future<Either<Error, List<Chapter>>> chapters = wrap(book, m).flatMap(b -> m.sequence(getChapters(b.getChapters()))).value();
+
+		Future<Either<Error, Summary>> summary = wrap(book, m)
+				.flatMap(
+						b -> m.map3(sales, author, chapters,
+						(s, a, c) -> new Summary(b, c, s, a)))
+				.recoverWith(error -> m.raiseError(new MyError("It is impossible to get book summary"))).value();
+
+		// Other ways of doing the same stuff
+		//
+		//
+		// return wrap(book, m).flatMap(
+		//		b -> m.flatMap(sales,
+		//		s -> m.flatMap(author,
+		//		a -> m.map(chapters,
+		//		c -> new Summary(b, c, Optional.of(s), a))))).value();
+		//
+		//
+		// return m.flatMap2(salesF, this.srvBook.getBook(idBook),
+		//			(sales, book) -> {
+		//				return m.map2(this.srvAuthor.getAuthor(book.getIdAuthor()), m.sequence(getChapters(book.getChapters())), (author, chapters) -> {
+		//					return new Summary(book, chapters, sales, author);
+		//				});
+		//			}
+		//	);
+		//
+		//
+		// return wrap(this.srvBook.getBook(idBook), m)
+		//		.flatMap(book -> wrap(
+		//				m.sequence(
+		//						book.getChapters().stream().map(chId -> this.srvChapter.getChapter(chId)).collect(Collectors.toList())
+		//				)
+		//				, m).flatMap(chapters ->  wrap( this.srvAuthor.getAuthor(book.getIdAuthor()), m).map(
+		//						author -> new Summary(book, chapters, sales, author)
+		//					).value()
+		//				).value()
+		//		).value();
+
+		return summary;
 	}
 
 	private List<Future<Either<Error, Chapter>>> getChapters(List<Long> chapters) {
